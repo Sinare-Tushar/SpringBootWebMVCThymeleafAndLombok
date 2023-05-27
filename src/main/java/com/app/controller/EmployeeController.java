268ff1d17f472ae1510fd0697b6b66a10d43a322
Sinare-Tushar <tusharsinare020295@gmail.com>
2023-05-27 12:54:21 +0530

    add changes

--- src/main/java/com/app/controller/EmployeeController.java
+++ src/main/java/com/app/controller/EmployeeController.java
@@ -2,13 +2,14 @@ package com.app.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 @Controller
 public class EmployeeController {
-	
+	Logger log = LogManager.getLogger(EmployeeController.class);
 @RequestMapping("reg")
 public String showEmployee() {
-	System.out.println("Hello How Are You");
+	log.info("Hello How Are You");
 	return "Register";
 }
 }
